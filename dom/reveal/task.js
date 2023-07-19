7ee8abf34d477c9a2b1c74d171c1796261a9cae3
Алексей Шацков <ashackov@bk.ru>
2023-07-19 15:44:37 +0300

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,23 @@
+function isElementInViewport(el) {
+	const rect = el.getBoundingClientRect();
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
+function revealOnScroll() {
+	const revealElements = document.querySelectorAll('.reveal');
+
+	revealElements.forEach(function(element) {
+		if (isElementInViewport(element)) {
+			element.classList.add('reveal_active');
+		}
+	});
+}
+
+window.addEventListener('scroll', revealOnScroll);
+
+revealOnScroll();
