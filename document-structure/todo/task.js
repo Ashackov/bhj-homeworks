da57d0f0dca67cfee5dfbee59f4d7df3d245275a
Алексей Шацков <ashackov@bk.ru>
2023-08-02 12:42:57 +0300

    first commit todo v2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,49 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const taskInput = document.getElementById("task__input");
-  const addButton = document.getElementById("tasks__add");
-  const taskList = document.getElementById("tasks__list");
+document.addEventListener("DOMContentLoaded", function() {
+	const taskInput = document.getElementById("task__input");
+	const addButton = document.getElementById("tasks__add");
+	const taskList = document.getElementById("tasks__list");
 
-  function addTask() {
-    const taskTitle = taskInput.value.trim();
-    if (taskTitle !== "") {
-      const taskDiv = document.createElement("div");
-      taskDiv.classList.add("task");
+	function addTask() {
+		const taskTitle = taskInput.value.trim();
+		if (taskTitle !== "") {
+			const taskDiv = document.createElement("div");
+			taskDiv.classList.add("task");
 
-      const taskTitleDiv = document.createElement("div");
-      taskTitleDiv.classList.add("task__title");
-      taskTitleDiv.textContent = taskTitle;
+			const taskTitleDiv = document.createElement("div");
+			taskTitleDiv.classList.add("task__title");
+			taskTitleDiv.textContent = taskTitle;
 
-      const removeButton = document.createElement("a");
-      removeButton.href = "#";
-      removeButton.classList.add("task__remove");
-      removeButton.innerHTML = "&times;";
+			const removeButton = document.createElement("a");
+			removeButton.href = "#";
+			removeButton.classList.add("task__remove");
+			removeButton.innerHTML = "&times;";
 
-      taskDiv.appendChild(taskTitleDiv);
-      taskDiv.appendChild(removeButton);
-      taskList.appendChild(taskDiv);
+			taskDiv.appendChild(taskTitleDiv);
+			taskDiv.appendChild(removeButton);
+			taskList.appendChild(taskDiv);
 
-      taskInput.value = "";
-    }
-  }
+			taskInput.value = "";
+		}
+	}
 
-  function removeTask(event) {
-    if (event.target.classList.contains("task__remove")) {
-      const taskDiv = event.target.parentElement;
-      taskList.removeChild(taskDiv);
-    }
-  }
+	function removeTask(event) {
+		if (event.target.classList.contains("task__remove")) {
+			const taskDiv = event.target.parentElement;
+			taskList.removeChild(taskDiv);
+		}
+	}
 
-  taskInput.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      addTask();
-    }
-  });
+	addButton.addEventListener("click", function(event) {
+		event.preventDefault();
+		addTask();
+	});
 
-  addButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    addTask();
-  });
-
-  taskList.addEventListener("click", removeTask);
+	taskList.addEventListener("click", removeTask);
 });
