80ca3469fcd24ff7ed125e61b7fb84bebe806c7e
Алексей Шацков <ashackov@bk.ru>
2023-08-10 17:23:01 +0300

    first commit auth v2

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,39 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const signinForm = document.getElementById("signin__form");
-	const welcomeBlock = document.getElementById("welcome");
-	const userIdSpan = document.getElementById("user_id");
+  const signinForm = document.getElementById("signin__form");
+  const welcomeBlock = document.getElementById("welcome");
+  const userIdSpan = document.getElementById("user_id");
 
-	const savedUserId = localStorage.getItem("user_id");
-	if (savedUserId) {
-		userIdSpan.textContent = savedUserId;
-		welcomeBlock.classList.add("welcome_active");
-	}
+  const savedUserId = localStorage.getItem("user_id");
+  if (savedUserId) {
+    userIdSpan.textContent = savedUserId;
+    welcomeBlock.classList.add("welcome_active");
+  }
 
-	signinForm.addEventListener("submit", async function(event) {
-		event.preventDefault();
+  signinForm.addEventListener("submit", async function(event) {
+    event.preventDefault();
 
-		const formData = new FormData(signinForm);
+    const formData = new FormData(signinForm);
 
-		try {
-			const response = await fetch("https://students.netoservices.ru/nestjs-backend/auth", {
-				method: "POST",
-				body: formData
-			});
+    try {
+      const response = await fetch("https://students.netoservices.ru/nestjs-backend/auth", {
+        method: "POST",
+        body: formData
+      });
 
-			const data = await response.json();
+      const data = await response.json();
 
-			if (data.success) {
-				const userId = data.user_id;
+      if (data.success) {
+        const userId = data.user_id;
 
-				localStorage.setItem("user_id", userId);
+        localStorage.setItem("user_id", userId);
 
-				userIdSpan.textContent = userId;
-				welcomeBlock.classList.add("welcome_active");
-			} else {
-				alert("Неверный логин/пароль");
-			}
-		} catch (error) {
-			console.error("Ошибка при авторизации", error);
-		}
-	});
+        userIdSpan.textContent = userId;
+        welcomeBlock.classList.add("welcome_active");
+	signinForm.style.display = "none";
+      } else {
+        alert("Неверный логин/пароль");
+      }
+    } catch (error) {
+      console.error("Ошибка при авторизации", error);
+    }
+  });
 });
