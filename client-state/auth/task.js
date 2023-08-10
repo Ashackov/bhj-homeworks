40291f8d032e176a2c53bd2bec64adbf2dbcf831
Алексей Шацков <ashackov@bk.ru>
2023-08-10 16:37:39 +0300

    first commit auth

---
+++ client-state/auth/task.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const signinForm = document.getElementById("signin__form");
+	const welcomeBlock = document.getElementById("welcome");
+	const userIdSpan = document.getElementById("user_id");
+
+	const savedUserId = localStorage.getItem("user_id");
+	if (savedUserId) {
+		userIdSpan.textContent = savedUserId;
+		welcomeBlock.classList.add("welcome_active");
+	}
+
+	signinForm.addEventListener("submit", async function(event) {
+		event.preventDefault();
+
+		const formData = new FormData(signinForm);
+
+		try {
+			const response = await fetch("https://students.netoservices.ru/nestjs-backend/auth", {
+				method: "POST",
+				body: formData
+			});
+
+			const data = await response.json();
+
+			if (data.success) {
+				const userId = data.user_id;
+
+				localStorage.setItem("user_id", userId);
+
+				userIdSpan.textContent = userId;
+				welcomeBlock.classList.add("welcome_active");
+			} else {
+				alert("Неверный логин/пароль");
+			}
+		} catch (error) {
+			console.error("Ошибка при авторизации", error);
+		}
+	});
+});
